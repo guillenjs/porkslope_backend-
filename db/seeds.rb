0e470bbd18ba82307923deb19aac990c093f569f
guillenjs <jorgeguilln@gmail.com>
2020-09-09 22:39:19 -0400

    set up server & set up some initial tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = [{
+    name:"fruit"
+},
+{
+    name:"cheese"
+}
+]
+
+
+items = [{
+  name: "apple",
+  price: 20,
+  category_id: 1
+},
+{
+  name: "banana",
+  price: 10,
+  category_id: 1
+},
+{
+  name: "kiwi",
+  price: 6,
+  category_id: 1
+},
+{
+  name: "pineapple",
+  price: 5,
+  category_id: 1
+},
+{
+  name: "mozarella",
+  price: 20,
+  category_id: 2
+},
+{
+  name: "brie",
+  price: 10,
+  category_id: 2
+},
+{
+  name: "oaxaca",
+  price: 6,
+  category_id: 2
+},
+{
+  name: "cheddar",
+  price: 5,
+  category_id: 2
+},
+]
+
+Category.create(categories)
+Item.create(items)
